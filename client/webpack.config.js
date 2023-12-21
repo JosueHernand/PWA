fe9ca793714c0dec513bbb28a07c2264d3c2b7b3
JosueHernand <joseh1337@gmail.com>
2023-12-21 01:45:55 -0800

    Added the plugin for webpack PWA manifest

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -28,6 +28,24 @@ module.exports = () => {
         swDest: 'src-sw.js'
       }),
 
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Progressive Web App',
+        short_name: 'PWA',
+        description: 'A simple PWA',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
 
     module: {
