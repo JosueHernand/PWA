b0b98d45f07d44afa113385f947a156adc679c41
JosueHernand <joseh1337@gmail.com>
2023-12-21 01:20:47 -0800

    Retrieve all content

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,7 +12,6 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
   const db = await initdb();
   const tx = db.transaction('WebFlowText', 'readwrite');
@@ -28,11 +27,20 @@ export const putDb = async (content) => {
   await tx.done;
 };
 
-// TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
   const db = await initdb();
   const tx = db.transaction('WebFlowText', 'readonly');
   const store = tx.objectStore('WebFlowText');
+
+  try {
+    const allContent = await store.getAll();
+    console.log('All content retrieved from the database:', allContent);
+    return allContent;
+  } catch (error) {
+    console.error('Error getting content from the database', error);
+  }
+
+  await tx.done;
 };
 
 initdb();
