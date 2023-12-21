c5c9507d48fbacfdc10b1342e86b67119aec2e80
JosueHernand <joseh1337@gmail.com>
2023-12-21 01:18:46 -0800

    Database and transaction setup

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -29,6 +29,10 @@ export const putDb = async (content) => {
 };
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction('WebFlowText', 'readonly');
+  const store = tx.objectStore('WebFlowText');
+};
 
 initdb();
