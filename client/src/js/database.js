63abb04eaaf49342413b83a7afac9332efcfe18c
JosueHernand <joseh1337@gmail.com>
2023-12-21 01:22:43 -0800

    Reverted the name back to webflowtext

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,14 +1,14 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('jate', 1, {
+  openDB('WebFlowText', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('jate')) {
-        console.log('jate database already exists');
+      if (db.objectStoreNames.contains('WebFlowText')) {
+        console.log('WebFlowText database already exists');
         return;
       }
-      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-      console.log('jate database created');
+      db.createObjectStore('WebFlowText', { keyPath: 'id', autoIncrement: true });
+      console.log('WebFlowText database created');
     },
   });
 
