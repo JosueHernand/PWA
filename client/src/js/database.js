449835723f8629debc394cb622549213b62a70c3
JosueHernand <joseh1337@gmail.com>
2023-12-21 01:06:08 -0800

    Start a transction and get the obeject store in the putDb method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,19 +1,23 @@
 import { openDB } from 'idb';
 
 const initdb = async () =>
-  openDB('WebFlowText', 1, {
+  openDB('jate', 1, {
     upgrade(db) {
-      if (db.objectStoreNames.contains('WebFlowText')) {
-        console.log('WebFlowText database already exists');
+      if (db.objectStoreNames.contains('jate')) {
+        console.log('jate database already exists');
         return;
       }
-      db.createObjectStore('WebFlowText', { keyPath: 'id', autoIncrement: true });
-      console.log('WebFlowText database created');
+      db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
+      console.log('jate database created');
     },
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await initdb();
+  const tx = db.transaction('WebFlowText', 'readwrite');
+  const store = tx.objectStore('WebFlowText');
+};
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => console.error('getDb not implemented');
