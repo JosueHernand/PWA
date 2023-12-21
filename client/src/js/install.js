7ef66e1c755e00d9d3a13dafff7a8f1004f6a705
JosueHernand <joseh1337@gmail.com>
2023-12-21 03:45:53 -0800

    Refactored the install file

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,25 +1,22 @@
 const butInstall = document.getElementById('buttonInstall');
 
-let deferredPrompt;
-
 window.addEventListener('beforeinstallprompt', (event) => {
-  event.preventDefault();
-  deferredPrompt = event;
-  butInstall.style.display = 'block';
+  window.deferredPrompt = event;
+  butInstall.classList.toggle('hidden', false);
 });
 
 butInstall.addEventListener('click', async () => {
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
-    const choiceResult = await defferedPrompt.userChoice;
-    if (choiceResult.outcome === 'accepted') {
-      console.log('User accepted the install prompt');
-    } else {
-      console.log('User dismissed the install prompt');
-    }
-    deferredPrompt = null;
-    butInstall.style.display = 'none';
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+    return;
   }
+
+  promptEvent.prompt();
+
+  window.deferredPrompt = null;
+
+  butInstall.classList.toggle('hidden', true);
 });
 
 window.addEventListener('appinstalled', (event) => {
